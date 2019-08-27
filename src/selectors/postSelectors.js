b5749328566a05e7900f586020da4f1fc22e24ef
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-27 09:59:22 -0700

    removed what I was just working on, I am leaving it where I am at

--- src/selectors/postSelectors.js
+++ src/selectors/postSelectors.js
@@ -1,2 +1,2 @@
-export const getPosts = state => state.posts.postsArray;
+export const getPosts = state => state.posts;
 export const getPostByIndex = (state, postIndex) => state.posts.postsArray[postIndex];
