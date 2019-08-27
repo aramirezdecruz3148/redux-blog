be66e73b20f39924eb937427662b423c41f7bda7
aramirezdecruz3148 <leigh3148@gmail.com>
2019-08-27 08:47:47 -0700

    I THINK that I figured out how to delete the comments that correspond to a post at least it works in the dev tools

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -1,4 +1,5 @@
 import { CREATE_COMMENT, DELETE_COMMENTS } from '../actions/commentActions';
+import { DELETE_POST } from '../actions/postActions';
 
 const initialState = {};
 
@@ -7,6 +8,11 @@ export default function reducer(state = initialState, action) {
     case CREATE_COMMENT:
       return { ...state, comments: { ...state.comments, [action.payload.postIndex]: [action.payload.comment] } };
     case DELETE_COMMENTS:
+      return { ...state, comments: { ...state.comments, [action.payload.postIndex]: [ 
+        ...state.comments[action.payload.postIndex].slice(0, action.payload.postIndex),
+        ...state.comments[action.payload.postIndex].slice(action.payload.postIndex + 1)
+      ] } };
+    case DELETE_POST:
       return { ...state, comments: { ...state.comments, [action.payload.postIndex]: [ 
         ...state.comments[action.payload.postIndex].slice(0, action.payload.postIndex),
         ...state.comments[action.payload.postIndex].slice(action.payload.postIndex + 1)
